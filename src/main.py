bad6c8fd4876ca2fe3a6223cfa1eebbaf2cdf58d
Lautaro Petaccio <lausuper@gmail.com>
2015-05-10 18:55:00 -0300

    ABM de mensajes

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from flask import Flask, request, redirect, url_for, render_template
 from app.campania import Campania
 from app.evento import Evento
 from app.repositorio_de_eventos import RepositorioDeEventos
+from app.repositorio_de_mensajes import RepositorioDeMensajes
 
 app = Flask(__name__)
 
@@ -66,9 +67,22 @@ def editar_campania(id):
 
 @app.route('/campanias/<id>/mensajes/')
 def mensajes(id):
-  #Conseguir lista de mensajes de la campaña
-  #Pasar como parámetro lista de mensajes
-  return render_template('mensajes.html')
+  lista_de_mensajes = []
+  nombre_campania = None
+  repositorio_de_mensajes = RepositorioDeMensajes.obtenerInstancia()
+  repositorio_de_eventos = RepositorioDeEventos.obtenerInstancia()
+  for evento in repositorio_de_eventos.eventos():
+    for campania in evento.campanias():
+      if str(campania) == id:
+        nombre_campania = campania.nombre()
+
+  for mensaje in repositorio_de_mensajes.mensajes():
+    if str(mensaje.campania()) == id:
+      lista_de_mensajes += [{'fecha' : mensaje.fecha(),
+                            'contenido' : mensaje.contenido()}]
+  
+  return render_template('mensajes.html', mensajes=lista_de_mensajes, id=id, 
+    nombre_campania=nombre_campania)
 
 @app.route('/campanias/<id>/mensajes/crear', methods=['GET', 'POST'])
 def crear_mensaje(id):
