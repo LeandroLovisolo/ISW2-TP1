0f85986b48faff62d10bc30078aa9353826330d4
Lautaro Petaccio <lausuper@gmail.com>
2015-05-10 15:52:31 -0300

    Clase evento

---
+++ src/app/evento.py
@@ -0,0 +1,22 @@
+from campania import Campania
+
+class Evento():
+  def __init__(self, unNombre, unAutor):
+    self._nombre = unNombre
+    self._autor = unAutor
+    self._campanias = list()
+
+  def nombre():
+    return _nombre
+
+  def nombre(unNombre):
+    _nombre = unNombre
+
+  def agregarCampania(unaCampania):
+    _campanias.append(unaCampania)
+
+  def quitarCampania(unaCampania):
+    _campanias.remove(unaCampania)
+
+  def campanias():
+    return _campanias
