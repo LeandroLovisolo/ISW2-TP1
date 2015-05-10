e5aee31b09052d30e1ed4a7208afdfc67a9dffc4
Leandro Lovisolo <leandro@leandro.me>
2015-05-10 16:01:58 -0300

    Repositorio de eventos

---
+++ src/app/repositorio_de_eventos.py
@@ -0,0 +1,21 @@
+class RepositorioDeEventos:
+
+  _instancia = None
+
+  @classmethod
+  def obtenerInstancia(cls):
+    if cls._instancia == None:
+      cls._instancia = RepositorioDeEventos()
+    return cls._instancia
+
+  def __init__(self):
+    self._eventos = []
+
+  def eventos(self):
+    return self._eventos
+
+  def agregarEventos(self, unEvento):
+    self._eventos.append(unEvento)
+
+  def quitarEvento(self, unEvento):
+    self._eventos.remove(unEvento)
